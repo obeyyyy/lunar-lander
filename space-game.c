107ccb8e94afb2b1ca0078fbbb78860926d091e7
oubaye el ghammat ghori <99280919+obeyyyy@users.noreply.github.com>
2022-05-19 14:38:33 +0100

    updating the gravity and acceleration logic

--- space-game.c
+++ space-game.c
@@ -7,43 +7,42 @@
 #define DELAY 30000
 
     
-
+void fuel(int *fuel)
+{
+*fuel=500;
+}
     
+
 int main(int argc, char *argv[])
 {
    
-
  initscr();
  noecho();
  curs_set(FALSE);
  bool GameOver =false;
  keypad(stdscr, TRUE); // enables arrows reading for controlls
  double X = 0;
- double *movX = &X; // new x position of the ship
+ // new x position of the ship
  double Y = 0;
- double *movY = &Y;// new y position of the ship
+ // new y position of the ship
  int MaxX=0; // maXx position to detect the end x position of the screen
  int MaxY=0; // maxYposition to detect the end y position of the screen
- double gr;
- gr = 0.01;
- 
- int score = 0;
+ getmaxyx(stdscr, MaxY,MaxX);  //function for getting the widnow size for stopping the ship to go through the window
  int boost = 8;
  char *ship = "A";
-
- char land[] = "_____/---/________|XX|___|~~~~~~~~|_________/";
+ char land[] = "_____/---/_5xscore________________|XX|_20xscore__________________|~~~WATER~~~~|______________________";
 
 while(GameOver != TRUE)
 {
 double acceleration;
+double gr; //gravity
 
-
-*movX +=gr; 
-gr+=0.01;;
-getmaxyx(stdscr, MaxY,MaxX);     //function for getting the widnow size for stopping the ship to go through the window
 clear();
-mvprintw(*movX,*movY,"%s", ship);       // prints the ship in an this form;//*
-mvprintw(20,1,land);
+gr= gr + 0.002; // increasing the gravity by the time goes
+X+=gr; // apply the gravity to the ship
+mvprintw(X,Y,"%s", ship); // prints the ship in an this form;//*
+mvprintw(40,1,land);
+//mvprintw(1,30,fuel())
 usleep(ESCDELAY);
 
 refresh();
@@ -57,15 +56,15 @@ input = getch();
 if (input == KEY_UP)
 {
      // if the up arrow is pressed, the ship accelerates up
-     gr-=0.01;
-     X-=gr;
-     acceleration+=0.02;
+     acceleration+=0.00005; 
      X-=acceleration;
+     gr-=acceleration; 
+     
 }
 
 if(input == KEY_RIGHT)
 {
-    Y  += 0.5;  // if the right arrow is pressed, the ship moves right
+    Y  +=0.5;  // if the right arrow is pressed, the ship moves right
 }
 if (input == KEY_LEFT)
 {
@@ -75,12 +74,9 @@ if(input == 'p')
 {
     GameOver = TRUE; // if p is pressed or A is out of range,GAMEOVER;
 }
-if(input == KEY_UP && input == KEY_RIGHT)
-{
-  
 
-}
-timeout(100);
+
+timeout(50);
 refresh();
 }
 endwin();
